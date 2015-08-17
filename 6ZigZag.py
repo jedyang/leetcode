02b6ccf9a660978c9dafeb21734748449b72e6ba
yunsheng <onlyyunsheng@126.com>
2015-08-17 19:18:55 +0800

    ZigZag Conversion
    ZigZag Conversion

---
+++ 6ZigZag.py
@@ -0,0 +1,29 @@
+class Solution:
+    # @param {string} s
+    # @param {integer} numRows
+    # @return {string}
+    # 这叫锯齿形的数据
+    ```
+    A     G     M
+    B   F H   L N
+    C E   I K   O
+    D     J     P
+    ```
+    def convert(self, s, numRows):
+        resultDict = {}
+        step = 2 * numRows - 2
+        if step == 0:
+            return s
+        for index in range(0, len(s)):
+            tempInt = index % step
+            if tempInt >= numRows:
+                tempInt = numRows - 1 - (tempInt - numRows + 1)
+            if not (tempInt in resultDict):
+                resultDict[tempInt] = []
+            tempList = resultDict.get(tempInt)
+            tempList.append(s[index])
+        values = resultDict.values()
+        result = []
+        for tempList in values:
+            result.extend(tempList)
+        return ''.join(result)
